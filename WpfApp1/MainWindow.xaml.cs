85774de75ee0adb2532d51be2ab81fa71d8f97aa
wei xue <360908495@qq.com>
2023-07-10 16:25:21 +0800

    update

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 2222; i++)
+            {
+                Task.Factory.StartNew(Work);
+            }
+            
+            
+
+        }
+
+        private void Work()
+        {
+            Task task = new Task((tb) => Begin(this.first), this.first);
+            Task task2 = new Task((tb) => Begin(this.second), this.first);
+            Task task3 = new Task((tb) => Begin(this.Three), this.first);
+            task.Start();
+            task.Wait();
+            task2.Start();
+            task2.Wait();
+            task3.Start();
+        }
+        private void Begin(TextBlock tb)
+        {
+            int i = 100000000;
+            while (i > 0)
+            {
+                i--;
+            }
+            Random random = new Random();
+
+            String Num = random.Next(0, 100).ToString();
+            Action<TextBlock, String> updateAction = new Action<TextBlock, string>(UpdateTb);
+            tb.Dispatcher.BeginInvoke(updateAction, tb, Num);
+        }
+
+        private void UpdateTb(TextBlock tb, string text)
+        {
+            tb.Text = text;
+        }
+    }
+}
--- image display/MainWindow.xaml.cs
+++ image display/MainWindow.xaml.cs
@@ -1,20 +1,14 @@
 ﻿using Emgu.CV;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace image_display
 {
@@ -26,9 +20,6 @@ namespace image_display
         public MainWindow()
         {
             InitializeComponent();
-
-
-
 
 
 
@@ -60,6 +51,8 @@ namespace image_display
         }
 
 
+
+
         public struct NIDAQ_Parameters
         {
             public string Name { set; get; }
@@ -88,34 +81,13 @@ namespace image_display
 
             public BitmapSource BSDisplay { set; get; }
 
-            public Bitmap bmp { set; get; }
+            public WriteableBitmap bmp { set; get; }
+
+            public ImageSource imageSource { set; get; }
         }
         public struct SampleStage
         {
-            //    public SampleStage()
-            //    {
-            //        Name = "";
-            //        X_position =222;//
-            //        X_initialVelocity = 222;
-
-            //    X_acceleration =222;//{ set; get; }
-            //        X_velocity = 222;//{ set; get; }
-
-            //        Y_position = 222;//{ set; get; }
-            //        Y_initialVelocity = 222;// { set; get; }
-            //        Y_acceleration = 222;//{ set; get; }
-            //        Y_velocity = 222;//{ set; get; }
-
-            //        Z_position = 222;//;
-            //        Z_initialVelocity = 222;//;
-            //        Z_acceleration = 222;//;
-            //        Z_velocity = 222;//;
-            //     axisNumber=2;
-
-
-            //    X= Y= Z=222;//位置
-            //}
-
+  
             public string Name;
             public int axisNumber;
 
@@ -152,25 +124,43 @@ namespace image_display
         ControlParameters imPara;
         Mat cvImage;
 
+
+        WriteableBitmap bmp;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-           
-            Thread thread=new Thread(ThreadStart);  
-            
-            
-            TEXTBLOCK.Text = "clicked";
+            System.DateTime currentTime = new System.DateTime();
+            int millisec0 = currentTime.Millisecond;
+
+            for (int i = 0; i < 100001; i++) { 
+                    
+                //
+                     Task.Factory.StartNew(Work);
+
+            }
+
+
+            Thread.Sleep(10000);
+
+            MessageBox.Show(Convert.ToString(updataNum));
 
 
 
+        }
+
+
+        private void Work()
+        {
+            Task task = new Task((image) => Begin(this.image), this.image);
+
+            task.Start();
+
+        }
+
+        private void Begin(System.Windows.Controls.Image img)
+        {
             Matrix<int> matrix8Bit = new Matrix<int>(512, 512, 3);
-
-
-            double voltageRange = 10;
-            double grayScacleRange = 65536;
-
-            //根据测量范围把double电压值转换为像素灰度值  
-
-            if (true) {  int[,] imgData = new int[512, 512];
+            int[,] imgData = new int[512, 512];
             for (int i = 0; i < 512; i++)
             {
                 for (int j = 0; j < 512; j++)
@@ -181,28 +171,38 @@ namespace image_display
                         imgData[i, j] = 0;
                     }
                     // matrix8Bit.Data.SetValue(Convert.ToInt32(imgData[i, j]), i, j);
-                    matrix8Bit.Data.SetValue(Convert.ToInt32(166), i, j);
+
+                    Random random = new Random();
+
+                    int Num = random.Next(0, 255);
+
+
+                    matrix8Bit.Data.SetValue(Convert.ToInt32(Num), i, j);
                 }
-            }
+            }//生产一幅图像
+
+             cvImage = matrix8Bit.Mat;
+
+            Action<System.Windows.Controls.Image, String> updateAction = new Action<System.Windows.Controls.Image, string>(UpdateTb);
+            img.Dispatcher.BeginInvoke(updateAction, img, "");
+        }
+
+        int updataNum=0;
+
+        private void UpdateTb(System.Windows.Controls.Image img, string text)
+        {
+            //tb.Text = text;
+
+            img.Source=cvImage.ToBitmapSource();
+
+            updataNum++;
+        }
 
 
 
-            cvImage = matrix8Bit.Mat;
-            //直方图校准
-            BitmapSource bs = cvImage.ToBitmapSource();
-            Bitmap bmp = cvImage.ToBitmap();
-            //BitmapImage bitmapImage = cvImage.;
-            //多线程调用
-            imPara.USB6351_galvoPMT.BSDisplay = cvImage.ToBitmapSource();
-            //imPara.USB6351_galvoPMT.BSDisplay = bmp;
-            Dispatcher.Invoke(new Action(() =>
-            {
-                imPara.USB6351_galvoPMT.bmp = cvImage.ToBitmap();
-            }));
-            image.Source = imPara.USB6351_galvoPMT.BSDisplay;
-       }
+        private void imageUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
 
-
-            }
+        }
     }
 }
